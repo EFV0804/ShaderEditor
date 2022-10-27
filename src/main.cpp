2cf460ebc5e78ec86f6b9965127476189f6b797d
EFV0804 <evidal@artfx.fr>
2022-10-27 12:33:21 +0200

    commented out Scene code, included Logger, added logging exemple function

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,46 @@
 #define GLFW_INCLUDE_NONE
+
 #include <GLFW/glfw3.h>
 #include "Renderer.h"
+#include "Logger.h"
 #include <string>
-#include "Scene.h"
 
+//#include "Scene.h"
+
+
+/*!
+ *\brief Example function to demonstrate how to use the logging functions.
+ */
+void loggingExample(){
+// ***************--- LOGGING EXAMPLE ---************//
+    int a = 5;
+    SD_RENDERER_INFO("Test info message VAR{0}", a);
+    SD_RENDERER_WARN("Test warning message");
+    SD_RENDERER_ERROR("Test error message");
+    SD_RENDERER_DEBUG("Test debug message");
+    SD_RENDERER_CRITICAL("Test critical message");
+    SD_INTERNAL_ASSERT_WITH_MSG(_RENDERER_, a>5, "a is inferior to 5");
+    SD_INTERNAL_ASSERT_NO_MSG(_RENDERER_, a>5);
+// **************************************************//
+}
 int main() {
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    Logger::init();
+    loggingExample();
 
     Renderer renderer;
     if (renderer.init() == EXIT_FAILURE) return EXIT_FAILURE;
-    Scene sceneExample{};
-    sceneExample.load(&renderer);
+//    Scene sceneExample{};
+//    sceneExample.load(&renderer);
 
-
-    while (!glfwWindowShouldClose(renderer.window))
-    {
+    while (!glfwWindowShouldClose(renderer.window)) {
         glfwPollEvents();
-        sceneExample.draw(&renderer);
+//        sceneExample.draw(&renderer);
+        renderer.draw();
     }
 
+    //TODO add to Renderer destructor
     renderer.cleanUp();
 
     return 0;
