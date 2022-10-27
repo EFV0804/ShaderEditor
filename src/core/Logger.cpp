77d8baa54215a2c1119fcd2d4cb6322a3437f620
EFV0804 <evidal@artfx.fr>
2022-10-27 12:27:40 +0200

    added logging function various levels, asserts and cmakelist lib for logger

--- src/core/Logger.cpp
+++ src/core/Logger.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+std::shared_ptr<spdlog::logger> Logger::rendererLogger;
+
+void Logger::init() {
+    spdlog::set_pattern("%^[%T] %n %v%$");
+    rendererLogger = spdlog::stdout_color_mt("RENDERER");
+    rendererLogger->set_level(spdlog::level::trace);
+}
--- src/core/Logger.h
+++ src/core/Logger.h
@@ -4,11 +4,40 @@
 
 #ifndef SHADEREDITOR_LOGGER_H
 #define SHADEREDITOR_LOGGER_H
+#include "spdlog/spdlog.h"
+#include <memory>
+#include <filesystem>
 
+#define VERBOSITY spdlog::level::trace
 
 class Logger {
+public:
+    Logger() = delete;
+    ~Logger() = delete;
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    static void init();
+    inline static std::shared_ptr<spdlog::logger>& getRendererLogger() {return rendererLogger;}
+
+//    static Logger* getLogger(){return instance;}
+
+private:
+    static std::shared_ptr<spdlog::logger> rendererLogger;
+//    static Logger* instance;
 
 };
+
+#define SD_RENDERER_TRACE(...) Logger::getRendererLogger()->trace(__VA_ARGS__)
+#define SD_RENDERER_DEBUG(...) Logger::getRendererLogger()->debug(__VA_ARGS__)
+#define SD_RENDERER_INFO(...) Logger::getRendererLogger()->info(__VA_ARGS__)
+#define SD_RENDERER_WARN(...) Logger::getRendererLogger()->warn(__VA_ARGS__)
+#define SD_RENDERER_ERROR(...) Logger::getRendererLogger()->error(__VA_ARGS__)
+#define SD_RENDERER_CRITICAL(...) Logger::getRendererLogger()->critical(__VA_ARGS__)
+
+
+#define SD_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { SD##type##ERROR(msg, __VA_ARGS__); __debugbreak(); } }
+#define SD_INTERNAL_ASSERT_WITH_MSG(type, check, ...) SD_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
+#define SD_INTERNAL_ASSERT_NO_MSG(type, check) SD_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", check, std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 
 #endif //SHADEREDITOR_LOGGER_H
