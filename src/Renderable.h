2446c12932ad22688967ecdcc3e036d010c2015f
EFV0804 <evidal@artfx.fr>
2022-11-15 16:15:04 +0100

    delete unused constructors and operator. Switched mesh and material to pointers. Added Getters.

--- src/Renderable.h
+++ src/Renderable.h
@@ -1,24 +1,25 @@
 //
 // Created by elise on 26/09/2022.
 //
+#pragma once
 
-#ifndef SHADEREDITOR_RENDERABLE_H
-#define SHADEREDITOR_RENDERABLE_H
 #include "Mesh.h"
 #include "Material.h"
 
 class Renderable {
 public:
-    Renderable() = default;
-    Renderable(Mesh pMesh, Material pMaterial);
+    Renderable() = delete;
+    Renderable &operator=(const Renderable&) = delete;
+    Renderable(Mesh* pMesh, Material* pMaterial);
     ~Renderable() = default;
 
+    Material* getMaterial() const {return material;}
+    Mesh* getMesh() const {return mesh;}
+
+private:
     const std::string meshName;
-    Mesh mesh{};
-    Material material;
+    Mesh* mesh;
+    Material* material;
 
     void load();
 };
-
-
-#endif //SHADEREDITOR_RENDERABLE_H
